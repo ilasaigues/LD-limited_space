35703d7fadf2054fbf988338f8be817df06eff0e
DEADSTAR\Nova <ilasaigues@gmail.com>
2023-10-02 15:45:23 -0300

    Animations working for all characters

--- Source/CharacterData.gd
+++ Source/CharacterData.gd
@@ -5,3 +5,5 @@ class_name CharacterData
 @export var texture:Texture2D
 @export var compatibleItems:Array[ItemData]
 @export var animationName:String
+enum SPECIAL_PROPERTY { None = 0,Fire=1, Ice=2, Acid=4}
+@export var element:SPECIAL_PROPERTY = SPECIAL_PROPERTY.None
--- Source/CharacterInstance.gd
+++ Source/CharacterInstance.gd
@@ -14,7 +14,16 @@ func _ready():
 		$AnimationTree.active = true
 		var id = "parameters/conditions/"+base_data.animationName
 		$AnimationTree[id] = true
-		
+	if base_data.element != base_data.SPECIAL_PROPERTY.None:
+		var elementalid = "parameters/conditions/"
+		match base_data.element:
+			base_data.SPECIAL_PROPERTY.Fire:
+				elementalid+=str( "fire_orc")
+			base_data.SPECIAL_PROPERTY.Ice:
+				elementalid+=str( "ice_orc")
+			base_data.SPECIAL_PROPERTY.Acid:
+				elementalid+=str( "poison_orc")
+		$AnimationTree[elementalid]=true
 	hp = base_data.maxHP
 	if item_slot:
 		item_slot.SetCharacterData(base_data)
